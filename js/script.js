4b90f9bd2603272f0d6379bb7cbcb86e795d695d
AlanFaraj83 <alanfaraj@outlook.com>
2023-04-02 14:05:45 -0300

    :sparkles: Atualizando o script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 // Cart
-let cartIcon = document.querySelector('#cart-icon');
-let cart = document.querySelector('.cart');
-let closeCart = document.querySelector('#close-cart');
+let cartIcon = document.querySelector("#cart-icon");
+let cart = document.querySelector(".cart");
+let closeCart = document.querySelector("#close-cart");
+
 
 // Open Cart
 cartIcon.onclick = () => {
@@ -15,10 +16,49 @@ closeCart.onclick = () => {
 
 
 // Cart Working JS
-if(document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded', ready);
+if(document.readyState == "loading") {
+    document.addEventListener("DOMContentLoaded", ready);
 } else {
     ready();
+}
+
+// Making Function 
+function ready () {
+// Remove Items from Cart
+    var removeCartButtons = document.getElementsByClassName("cart-remove");
+    console.log(removeCartButtons);
+    for (var i= 0; i < removeCartButtons.length; i++) {
+    var button = removeCartButtons[i];
+    button.addEventListener("click", removeCartItem);
+ }
+ // Quantity Changes
+ 
+
+
+}
+
+// Remove Items From Cart
+function removeCartItem(event) {
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.remove();
+    updatetotal();
+}
+
+// Update Total
+function updatetotal() {
+    var cartContent = document.getElementsByClassName('cart-content')[0];
+    var cartBoxes = cartContent.getElementsByClassName('cart-box');
+    var total = 0;
+    for (var i= 0; i < cartBoxes.length; i++) {
+        var cartBox = cartBoxes[i];
+        var priceElement = cartBox.getElementsByClassName('cart-price')[0];
+        var quantityElement = cartBox.getElementsByClassName('cart-quantity')[0];
+        var quantity = quantityElement.value;
+        var price = parseFloat(priceElement.innerText.replace("$", "" ));
+        total = total + (price * quantity); 
+
+        document.getElementsByClassName('total-price')[0].innerText = '$' + total;
+    }
 }
 
 
